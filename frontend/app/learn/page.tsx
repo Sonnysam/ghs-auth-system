57ffaebbb98a3141cd55ede84c961a3bc8da964c
Paywinful <fiifipayin@gmail.com>
2024-06-20 01:20:14 +0000

    Learn made responsive

--- frontend/app/learn/page.tsx
+++ frontend/app/learn/page.tsx
@@ -20,20 +20,20 @@ const Learn = () => {
                 </p>
             </div>
         </div>
-       <div className='flex items-center justify-evenly py-28'>
-       <div className="card w-1/4">
+       <div className='md:flex md:items-center justify-center md:justify-evenly py-28'>
+       <div className="card md:w-1/4 ">
             <div className='text-center text-white text-xs p-6'>
                 <p className='text-xl p-2'>Backgound</p>
                 The 1992 Constitution of the Republic of Ghana (Chapter 14) provided for the establishment of the Ghana Health Service (GHS) as part of the Public Services of Ghana. In 1996, Parliament passed the Ghana Health Service and Teaching Hospitals Act 1996, Act 525, to pave way for the establishment of GHS.The GHS is therefore a Public Service body established under Act 525 of 1996 as required by the 1992 constitution. It is an autonomous Executive Agency responsible for implementation of national policies under the control of the Minister for Health through its governing Council – the Ghana Health Service Council.
             </div>
         </div>
-        <div className="card w-1/4">
+        <div className="card md:w-1/4">
             <div className='text-center text-white text-xs p-6'>
                 <p className='text-xl p-2'>Rationale</p>
                 The establishment of the GHS was an essential part of the key strategies identified in the Health Sector Reform process in the 1990s, which was outlined in the Medium-Term Health Strategy and Five Year Programme of Work for the period 1997-2001. The strategies were necessary steps in establishing a more equitable, efficient, accessible and responsive health care system. The reforms built on the reorganization of the MOH that began in 1993, which was explicitly designed to set the scene for the establishment of the GHS. The reforms also provided a sound organizational framework for the growing degree of managerial responsibility that had already been delegated to districts and hospitals. Themes that were central to the reorganization of 1993 remain important today for GHS: careful stewardship of scare resources, clear lines of responsibility and control, decentralization, and accountability for performance rather than inputs.
             </div>
         </div>
-        <div className="card w-1/4">
+        <div className="card md:w-1/4">
             <div className='text-center text-white text-xs p-6'>
                 <p className='text-xl p-2'>Objectives</p>
                     Implement approved national policies for health delivery in the country.
